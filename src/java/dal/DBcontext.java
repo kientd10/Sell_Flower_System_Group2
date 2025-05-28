7f9a7e7a9846549a70b53fd94569f056259c0324
tuHE180023tc <tutche180023@fpt.edu.vn>
2025-05-28 08:36:44 +0700

    new

---
+++ src/java/dal/DBcontext.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author tuanh
+ */
+public class DBContext {
+    private static final String URL = "jdbc:mysql://localhost:3306/flower_shop_db";
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "123456"; 
+
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL Driver not found", e);
+        }
+    }
+
+    public void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
