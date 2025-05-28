b4bf218510e2c00fe9d3b5ac58441d0634e27937
thang363 <thangbdhe187283@fpt.edu.vn>
2025-05-28 09:16:55 +0700

    sell flower system

--- src/java/dal/DBcontext.java
+++ src/java/dal/DBcontext.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
 public class DBContext {
     private static final String URL = "jdbc:mysql://localhost:3306/flower_shop_db";
     private static final String USER = "root"; 
-    private static final String PASSWORD = "123456"; 
+    private static final String PASSWORD = "1234"; 
 
     public Connection getConnection() throws SQLException {
         try {
