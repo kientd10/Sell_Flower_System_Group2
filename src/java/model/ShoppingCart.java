545845ef51531750b31f8e8faff7cc654f5f2d26
nguyenkhacdat994 <datnkhe171634@fpt.edu.vn>
2025-05-30 22:52:56 +0700

    Update ShoppingCart.java

--- src/java/model/ShoppingCart.java
+++ src/java/model/ShoppingCart.java
@@ -1,57 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package model;
 
-/**
- *
- * @author PC
- */
-import java.util.*;
-import java.lang.*;
 import java.time.LocalDateTime;
+
 public class ShoppingCart {
-   private int Cart_id;
-   private int User_id;
-   private int Template_id;
-   private int Quantity;
-   private LocalDateTime Add_at;
+
+    private int CartId;
+    private int UserId;
+    private Product Product;
+    private int Quantity;
+    private LocalDateTime AddedAt;
 
     public ShoppingCart() {
     }
 
-    public ShoppingCart(int Cart_id, int User_id, int Template_id, int Quantity, LocalDateTime Add_at) {
-        this.Cart_id = Cart_id;
-        this.User_id = User_id;
-        this.Template_id = Template_id;
+    public ShoppingCart(int CartId, int UserId, Product Product, int Quantity, LocalDateTime AddedAt) {
+        this.CartId = CartId;
+        this.UserId = UserId;
+        this.Product = Product;
         this.Quantity = Quantity;
-        this.Add_at = Add_at;
+        this.AddedAt = AddedAt;
     }
 
-    public int getCart_id() {
-        return Cart_id;
+    public int getCartId() {
+        return CartId;
     }
 
-    public void setCart_id(int Cart_id) {
-        this.Cart_id = Cart_id;
+    public void setCartId(int CartId) {
+        this.CartId = CartId;
     }
 
-    public int getUser_id() {
-        return User_id;
+    public int getUserId() {
+        return UserId;
     }
 
-    public void setUser_id(int User_id) {
-        this.User_id = User_id;
+    public void setUserId(int UserId) {
+        this.UserId = UserId;
     }
 
-    public int getTemplate_id() {
-        return Template_id;
+    public Product getProduct() {
+        return Product;
     }
 
-    public void setTemplate_id(int Template_id) {
-        this.Template_id = Template_id;
+    public void setProduct(Product Product) {
+        this.Product = Product;
     }
 
     public int getQuantity() {
@@ -62,12 +53,16 @@ public class ShoppingCart {
         this.Quantity = Quantity;
     }
 
-    public LocalDateTime getAdd_at() {
-        return Add_at;
+    public LocalDateTime getAddedAt() {
+        return AddedAt;
     }
 
-    public void setAdd_at(LocalDateTime Add_at) {
-        this.Add_at = Add_at;
+    public void setAddedAt(LocalDateTime AddedAt) {
+        this.AddedAt = AddedAt;
     }
-   
+
+    public void add(ShoppingCart line) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
