5c58dc1ee6711d3ef2c4923d53dea1d79ef4db57
nguyenkhacdat994 <datnkhe171634@fpt.edu.vn>
2025-05-30 22:51:54 +0700

    Update ProductDao.java

--- src/java/dal/ProductDao.java
+++ src/java/dal/ProductDao.java
@@ -5,27 +5,39 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 import model.Product;
 import model.ShoppingCart;
 
 public class ProductDao extends DBContext {
-    public List<ShoppingCart> getCartByUserID(int user_id) {
+
+    public List<ShoppingCart> getCartItemsByUserId(int userId) {
         List<ShoppingCart> cartItems = new ArrayList<>();
-        String sql = "SELECT d.quantity, d.added_at "
-                   + "FROM users u "
-                   + "JOIN shopping_cart d ON u.user_id = d.user_id "
-                   + "JOIN bouquet_templates bt ON d.template_id = bt.template_id "
-                   + "WHERE u.user_id = ?";
+        String sql
+                = "SELECT d.cart_id, d.user_id, d.template_id, d.quantity, d.added_at, bt.stock , "
+                + "bt.template_name, bt.description, bt.base_price, bt.image_url "
+                + "FROM shopping_cart d "
+                + "JOIN bouquet_templates bt ON d.template_id = bt.template_id "
+                + "WHERE d.user_id = ?";
 
         try (PreparedStatement st = connection.prepareStatement(sql)) {
-            st.setInt(1, user_id);
-            try (ResultSet rs = st.executeQuery()) {
-                while (rs.next()) {
-                    ShoppingCart h = new ShoppingCart();
-                    h.setQuantity(rs.getInt("quantity"));
-                    h.setAdd_at(rs.getTimestamp("added_at").toLocalDateTime());
-                    cartItems.add(h);
-                }
+            st.setInt(1, userId);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Product p = new Product();
+                p.setTemplateId(rs.getInt("template_id"));
+                p.setTemplateName(rs.getString("template_name"));
+                p.setDescription(rs.getString("description"));
+                p.setBasePrice(rs.getDouble("base_price"));
+                p.setImageUrl(rs.getString("image_url"));
+                p.setStock(rs.getInt("stock"));
+                ShoppingCart line = new ShoppingCart();
+                line.setCartId(rs.getInt("cart_id"));
+                line.setUserId(rs.getInt("user_id"));
+                line.setProduct(p);
+                line.setQuantity(rs.getInt("quantity"));
+                line.setAddedAt(rs.getTimestamp("added_at").toLocalDateTime());
+                cartItems.add(line);
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -33,30 +45,68 @@ public class ProductDao extends DBContext {
         return cartItems;
     }
 
-    public List<Product> getProductsByUserID(int user_id) {
-        List<Product> products = new ArrayList<>();
-        String sql = "SELECT bt.template_name, bt.description, bt.base_price, bt.image_url "
-                   + "FROM users u "
-                   + "JOIN shopping_cart d ON u.user_id = d.user_id "
-                   + "JOIN bouquet_templates bt ON d.template_id = bt.template_id "
-                   + "WHERE u.user_id = ?";
+    public ShoppingCart getItems(int userId, int templateId) {
+        ShoppingCart Item = new ShoppingCart();
+        String sql
+                = "SELECT d.cart_id, d.user_id, d.template_id, d.quantity, d.added_at, bt.stock , "
+                + "bt.template_name, bt.description, bt.base_price, bt.image_url "
+                + "FROM shopping_cart d "
+                + "JOIN bouquet_templates bt ON d.template_id = bt.template_id "
+                + "WHERE d.user_id = ? and bt.template_id = ?";
 
         try (PreparedStatement st = connection.prepareStatement(sql)) {
-            st.setInt(1, user_id);
-            try (ResultSet rs = st.executeQuery()) {
-                while (rs.next()) {
-                    Product h = new Product();
-                    h.setTemplateName(rs.getString("template_name"));
-                    h.setDescription(rs.getString("description"));
-                    h.setBasePrice(rs.getDouble("base_price"));
-                    h.setImageUrl(rs.getString("image_url"));
-                    products.add(h);
-                }
+            st.setInt(1, userId);
+            st.setInt(2, templateId);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Product p = new Product();
+                p.setTemplateId(rs.getInt("template_id"));
+                p.setTemplateName(rs.getString("template_name"));
+                p.setDescription(rs.getString("description"));
+                p.setBasePrice(rs.getDouble("base_price"));
+                p.setImageUrl(rs.getString("image_url"));
+                p.setStock(rs.getInt("stock"));
+                Item.setCartId(rs.getInt("cart_id"));
+                Item.setUserId(rs.getInt("user_id"));
+                Item.setProduct(p);
+                Item.setQuantity(rs.getInt("quantity"));
+                Item.setAddedAt(rs.getTimestamp("added_at").toLocalDateTime());
+
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return products;
+        return Item;
+    }
+
+    public void deleteCartItemsByCartId(int CartId) {
+        String sql = "DELETE FROM shopping_cart WHERE cart_id = ?";
+        try (PreparedStatement st = connection.prepareStatement(sql)) {
+            st.setInt(1, CartId);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateQuantityAndStock(int userId, int templateId, int newQuantity) throws SQLException {
+        ShoppingCart oldItem = getItems(userId, templateId);
+        int oldQuantity = oldItem.getQuantity();
+        int delta = newQuantity - oldQuantity;
+        String sql
+                = "UPDATE shopping_cart sc "
+                + " JOIN bouquet_templates bt ON sc.template_id = bt.template_id "
+                + "SET sc.quantity = ?, "
+                + "    bt.stock    = bt.stock - ? "
+                + "WHERE sc.user_id     = ? "
+                + "  AND sc.template_id = ?";
+        try (PreparedStatement st = connection.prepareStatement(sql)) {
+            st.setInt(1, newQuantity);  
+            st.setInt(2, delta);        
+            st.setInt(3, userId);
+            st.setInt(4, templateId);
+            st.executeUpdate();
+        }
     }
 
 }
