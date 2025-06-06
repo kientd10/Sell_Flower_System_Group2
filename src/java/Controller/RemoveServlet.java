949a572c17d8a9337f81b0f53116bb0c20fe6b1b
nguyenkhacdat994 <datnkhe171634@fpt.edu.vn>
2025-06-07 02:21:23 +0700

    Update cart and edit profile

--- src/java/Controller/RemoveServlet.java
+++ src/java/Controller/RemoveServlet.java
@@ -2,54 +2,50 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
+
 package Controller;
 
-import dal.UserDAO;
+import dal.BouquetDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.Date;
-import Model.User;
 
 /**
  *
  * @author PC
  */
-public class EditProfileServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
+public class RemoveServlet extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet EditProfileServlet</title>");
+            out.println("<title>Servlet RemoveServlet</title>");  
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet EditProfileServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet RemoveServlet at " + request.getContextPath () + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    }
+    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
+    /** 
      * Handles the HTTP <code>GET</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -57,20 +53,15 @@ public class EditProfileServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        User user = (User) request.getSession().getAttribute("user");
-        int user_id = user.getUserId();
-        UserDAO dao = new UserDAO();
-        User use = dao.getInfoUserByID(user_id);
-        request.setAttribute("user", use);
-        String mode = request.getParameter("mode");
-        request.setAttribute("editMode", mode != null && mode.equals("edit"));
-        request.getRequestDispatcher("ProfilePaging.jsp").forward(request, response);
-    }
+    throws ServletException, IOException {
+        int cartID = Integer.parseInt(request.getParameter("cartId"));
+        BouquetDAO c = new BouquetDAO();
+        c.deleteCartItemsByCartId(cartID);
+        response.sendRedirect("cart");
+    } 
 
-    /**
+    /** 
      * Handles the HTTP <code>POST</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -78,26 +69,12 @@ public class EditProfileServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        User user = (User) request.getSession().getAttribute("user");
-        int userID= user.getUserId();
-        String fullname = request.getParameter("name");
-        String email = request.getParameter("email");
-        String address = request.getParameter("address");
-        String phone = request.getParameter("phone");
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        UserDAO dao = new UserDAO();
-        dao.changeInfoUserByID(userID, username, fullname, email, address, phone,password);
-        User use = dao.getInfoUserByID(userID);
-        request.setAttribute("user", use);
-        request.setAttribute("editMode", false);
-        request.getRequestDispatcher("ProfilePaging.jsp").forward(request, response);
+    throws ServletException, IOException {
+        processRequest(request, response);
     }
 
-    /**
+    /** 
      * Returns a short description of the servlet.
-     *
      * @return a String containing servlet description
      */
     @Override
